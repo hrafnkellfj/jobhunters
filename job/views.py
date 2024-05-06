129cf0e9852de3e3e0ffb3e5653d83ff04359058
Rannaf <112666574+Rannaf@users.noreply.github.com>
2024-05-06 21:42:23 +0000

    job views og job html
    harðkóðuð gögn í views, nota for loop í html

--- job/views.py
+++ job/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+jobs = [
+    {'title': 'Meindýraeyðir', 'location': 'Reykjavík'},
+    {'title': 'Nuddari', 'location': 'Eskifjörður'}]
+    #serverdót
+    #jid
+    #cid
+    #catid
 
 def index(request):
-    return render(request, 'job/index.html')
+    return render(request, 'job/index.html', context={'jobs': jobs})
 
