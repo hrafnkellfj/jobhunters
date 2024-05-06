6498bc0f52a5f57d7732110d561dbef737eb1307
hrafnkellfj <hrafnkell22@ru.is>
2024-05-06 15:48:00 +0000

    second commit

---
+++ applicants/views.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the applicants index.")
+
+
+
+# Create your views here.
