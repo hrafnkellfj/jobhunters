80868884c3ca115819621c362bd485a13a86b71f
dagurmo <112622926+dagurmo@users.noreply.github.com>
2024-05-08 16:19:46 +0000

    Application steps 1-4 breytt í model form

--- applicant/models.py
+++ applicant/models.py
@@ -1,31 +1,31 @@
 from django.db import models
 
+
 class ApplicantCountry(models.Model):
     """Allows the applicant class to link the applicant
     to a country that's in our database"""
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Applicant(models.Model):
     """This class contains all the variables and methods that a user of type Applicant will need"""
+
     #grunnur
     name = models.CharField(max_length=255)
     photo = models.CharField(max_length=100, blank=True, null=True)
     username = models.CharField(max_length=30, unique=True)
+
     #auka frá okkur
     email = models.EmailField(max_length=255, unique=True)
     aboutMe = models.CharField(max_length=1000, blank=True, null=True)
     phone = models.IntegerField(blank=True, null=True)
+
     #Autofillast í job application ef þetta er fyllt út
     street = models.CharField(max_length=255, blank=True, null=True)
     houseNr = models.CharField(max_length=10, blank=True, null=True)
     city = models.CharField(max_length=30, blank=True, null=True)
-    country = models.ForeignKey(ApplicantCountry, blank=True, null=True, on_delete=models.CASCADE) #!Valið með select html úr lista
+    country = models.ForeignKey(ApplicantCountry, blank=True, null=True,on_delete=models.CASCADE)  #!Valið með select html úr lista
     postalCode = models.CharField(max_length=255, blank=True, null=True)
-
-
-
-
-
-
-
