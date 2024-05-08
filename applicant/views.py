5c30ea348d6281eaa7a2fe3ab8ecfcaed4dca45c
hrafnkellfj <hrafnkell22@ru.is>
2024-05-08 11:08:07 +0000

    summary

--- applicant/views.py
+++ applicant/views.py
@@ -5,6 +5,26 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'applicant/index.html')
 
+def application1(request):
+    return render(request, 'applicant/applyToJob_step1.html')
+
+
+def application2(request):
+    return render(request, 'applicant/applyToJob_step2.html')
+
+
+def application3(request):
+    return render(request, 'applicant/applyToJob_step3.html')
+
+
+
+def application4(request):
+    return render(request, 'applicant/applyToJob_step4.html')
+
+
+
+def mottekinUmsokn(request):
+    return render(request, 'applicant/mottekinUmsokn.html')
 
 
 # Create your views here.
