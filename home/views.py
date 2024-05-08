95b1960ec10cafd915db53b333e22f76638e0c05
hrafnkellfj <hrafnkell22@ru.is>
2024-05-08 09:22:12 +0000

    Laga about_us

--- home/views.py
+++ home/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'home/index.html')
 
-
+def about_us(request):
+    return render(request, 'home/about_us.html')
 def login(request):
     return render(request, 'home/login.html')
 
