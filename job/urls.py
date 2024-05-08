25cfd76d2f5686dbba474132ddd8e981840ad2f3
Rannaf <112666574+Rannaf@users.noreply.github.com>
2024-05-08 14:25:55 +0000

    hægt að ýta á company innan job details til að komast á company page

--- job/urls.py
+++ job/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:id>', views.get_job_by_id, name="job_details")
+    path('<int:id>', views.get_job_by_id, name="job_details"),
+
 ]
