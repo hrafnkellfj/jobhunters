a646a233dd435718efe09880013f18e764872403
Rannaf <112666574+Rannaf@users.noreply.github.com>
2024-05-09 10:43:19 +0000

    addaði field of study

--- applicant/models.py
+++ applicant/models.py
@@ -29,10 +29,12 @@ class Applicant(models.Model):
     country = models.ForeignKey(ApplicantCountry, blank=True, null=True,on_delete=models.CASCADE)  #!Valið með select html úr lista
     postalCode = models.CharField(max_length=255, blank=True, null=True)
 
+
 class ApplicantEduction(models.Model):
     """This class holds information about the education of the applicant."""
     applicant = models.ForeignKey(Applicant, on_delete=models.CASCADE)
     school = models.CharField(max_length=255)
     degree = models.CharField(max_length=255)
+    fieldOfStudy = models.CharField(max_length=255,)
     dateStart = models.DateField()
     dateEnd = models.DateField()
