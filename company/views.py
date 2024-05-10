27efacfa69654dd25f304a0a2fccaa00ee9a5a97
Rannaf <112666574+Rannaf@users.noreply.github.com>
2024-05-10 09:46:36 +0000

    færði meira af logic úr views í model

--- company/views.py
+++ company/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from company.models import Company
 from job.models import Job
+from datetime import date
 #from django.http import HttpResponse
 
 
@@ -10,8 +11,7 @@ def index(request):
     return render(request, 'company/index.html', all_companies)
 
 def get_company_by_id(request, id):
-    all_company_jobs = Job.objects.filter(company_id=id)
-    valid_company_jobs = [job for job in all_company_jobs if not job.isDue()]
+    all_company_jobs = Job.objects.filter(company_id=id, dueDate__gt=date.today())
     return render(request, 'company/company_details.html', {
-      'company': get_object_or_404(Company, pk=id), 'company_jobs': valid_company_jobs
+      'company': get_object_or_404(Company, pk=id), 'company_jobs': all_company_jobs
     })
