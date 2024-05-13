428429ee39eb94602837af5baed4e04ce34cc00e
Rannaf <112666574+Rannaf@users.noreply.github.com>
2024-05-13 15:54:28 +0000

    navbar

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import applicantProfile
+from .models import companyProfile
 
 # Register your models here.
+admin.site.register(applicantProfile)
+admin.site.register(companyProfile)
