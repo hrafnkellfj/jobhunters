18485b55e455031cf42dd3fdf4ee375eff49d7ca
hrafnkellfj <hrafnkell22@ru.is>
2024-05-15 09:04:20 +0000

    css og login

--- job/migrations/0018_alter_recommendation_allowedtocontact.py
+++ job/migrations/0018_alter_recommendation_allowedtocontact.py
@@ -15,7 +15,6 @@ class Migration(migrations.Migration):
 
             field=models.BooleanField(default=False),
 
-            field=models.BooleanField(blank=True, default=False),
 
         ),
     ]
